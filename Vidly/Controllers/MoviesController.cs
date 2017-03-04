5059feb9fc80e121a3d92a99f6c4b972adee7a69
krisSzell <skorupa.krzysztof1@gmail.com>
2017-03-04 12:39:43 +0100

    Make new View for user without edition permissions
    Duplicated Index View of Movies and changed logic in Movies Controller
    Index action to redirect to specific view depending on user role

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -24,8 +24,10 @@ namespace Vidly.Controllers
         // GET: Movies
         public ViewResult Index()
         {
- 
-            return View();
+            if (User.IsInRole("CanManageMovies"))
+                return View("List");
+
+            return View("ReadOnlyList");
 
         }
 
